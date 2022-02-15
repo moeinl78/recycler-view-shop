7136609ccd19731a4be16d6f16a9873cb7869a95
Moein Latifi <moeinl1090@protonmail.com>
2022-02-15 22:53:47 +0330

    created adapter for recycler view

---
+++ app/src/main/java/ir/ariyana/techshop/Adapter.kt
@@ -0,0 +1,36 @@
+package ir.ariyana.techshop
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ir.ariyana.techshop.databinding.ItemCardBinding
+
+class Adapter(private val data : ArrayList<Item>,) : RecyclerView.Adapter<Adapter.ViewHolder>(){
+
+    inner class ViewHolder(private val binding : ItemCardBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindData(position : Int) {
+            binding.itemName.text = data[position].itemName
+            binding.itemWeight.text = data[position].itemWeigh
+            binding.itemOs.text = data[position].itemSoftware
+            binding.itemReleaseDate.text = data[position].itemReleaseDate
+            Glide
+                .with(binding.root.context)
+                .load(data[position].itemImage)
+                .into(binding.itemImageView)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindData(position)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
---
+++ app/src/main/java/ir/ariyana/techshop/Item.kt
@@ -0,0 +1,9 @@
+package ir.ariyana.techshop
+
+data class Item(
+    val itemName : String,
+    val itemWeigh : String,
+    val itemSoftware : String,
+    val itemReleaseDate : String,
+    val itemImage : String,
+)
